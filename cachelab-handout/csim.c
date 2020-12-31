0d90b3ab086cfd646cd03b307656cf03436b8390
thasinaz <76603398+thasinaz@users.noreply.github.com>
2020-12-31 17:20:13 +0800

    Fix bug

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -276,7 +276,7 @@ int main(int argc, char *argv[]) {
         return 0;
     }
     if (fd == NULL) {
-        printf("cannot open file\n");
+        fprintf(stderr, "cannot open file\n");
         exit(1);
     }
 
